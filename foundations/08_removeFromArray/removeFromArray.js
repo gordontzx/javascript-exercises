44037729a29b61cfea86b59dcf57d4b7cc21cbbb
gordontzx <gordontaylol@gmail.com>
2026-01-03 01:11:19 +0800

    Solve 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...toRemove) {
+    toRemove = new Set(toRemove);
+    const res = [];
+    for (const num of arr) {
+        if (!toRemove.has(num)) {
+            res.push(num);
+        }
+    }
+    return res;
 };
 
 // Do not edit below this line
