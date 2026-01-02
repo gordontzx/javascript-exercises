f32bae1a6fc87bddbb27fe3af749ace25dff0ca9
gordontzx <gordontaylol@gmail.com>
2026-01-03 01:07:12 +0800

    Solve 06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(s, times) {
+    if (times < 0)
+        return "ERROR";
 
+    const res = [];
+    for (let i = 0; i < times; i++) {
+        res.push(s);
+    }
+    return res.join("");
 };
 
 // Do not edit below this line
