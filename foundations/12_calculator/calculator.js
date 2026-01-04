3d4f589357f04e9185ba8f93c176174fc4476c34
gordontzx <gordontaylol@gmail.com>
2026-01-05 03:34:04 +0800

    Solve 12_calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,33 +1,37 @@
-const add = function() {
-	
+const add = function(x, y) {
+    return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+    return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+    return nums.reduce(add, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+    return nums.reduce((a, b) => a * b, 1);
 };
 
-const power = function() {
-	
+const power = function(b, e) {
+    return b ** e;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+    let res = 1;
+    for (let i = 2; i <= n; i++) {
+        res *= i;
+    }
+    return res;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial
 };
