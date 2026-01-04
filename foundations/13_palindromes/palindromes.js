13992484827f07bbcce692c4aaf13e1ee5e203bb
gordontzx <gordontaylol@gmail.com>
2026-01-05 03:49:40 +0800

    Solve 14_fibonacci

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (s) {
+    const skip = ",.!?'\" ";
+    const t = s.split("").filter(c => !skip.includes(c)).map(c => c.toLowerCase());
+    console.log(t);
 
+    for (let l = 0, r = t.length - 1; l < r; l++, r--) {
+        if (t[l] !== t[r]) {
+            return false;
+        }
+    }
+
+    return true;
 };
 
 // Do not edit below this line
