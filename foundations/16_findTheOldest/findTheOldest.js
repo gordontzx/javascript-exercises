6b96dc1f8d7c2eba3f93e322e8911225f3a74acb
gordontzx <gordontaylol@gmail.com>
2026-01-05 03:58:58 +0800

    Solve 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const pair = people.reduce((oldest, p) => {
+        const deathYear = p.yearOfDeath ? p.yearOfDeath : new Date().getFullYear();
+        const age = deathYear - p.yearOfBirth;
+        return age > oldest[0] ? [age, p] : oldest;
+    }, [0, null]);
+    
+    return pair[1];
 };
 
 // Do not edit below this line
